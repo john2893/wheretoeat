4d2d068bbbda11cd23a1702fab66cf9045f3f41c
johns <johns@wfu.edu>
2021-11-04 16:47:26 -0400

    Add js code

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 
-
-// const request = require('request')
-// const fetch = require('node-fetch');
-// import { fetch } from "node-fetch";
-import axios from 'axios';
 // Place holder for Yelp Fusion's API Key. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
 const apiKey = 'MbMprHFPR_z5vwXJMwhXvn7lp3r05wDOJkAfByqxxN6U2PfI2XsPV6flgAW13YTbOyFoQyJGo98ygtOS8661ejHQ2Ws1iy3maW82HXeRQ5atFZSSPRYJuVxINFe_W3Yx';
@@ -11,19 +6,12 @@ const url='https://api.yelp.com/v3/businesses/search'
 
 import yelpAPI from 'yelp-api'
 
-// Create a new yelpAPI object with your API key
 
 let yelp = new yelpAPI(apiKey);
-
-// Set any parameters, if applicable (see API documentation for allowed params)
 let params = [{ location: 'winston salem' }];
 
-// Call the endpoint
 yelp.query('businesses/search', params)
 .then(data => {
-  //Success
-//  console.log(data);
-    // console.log(data)
     var d = JSON.parse(data)
     // console.log(d["businesses"])
     console.log(d["businesses"][0].name)
