b8e59369af21a49b492a784eeb8d598359b2a9aa
sujith john <sujithjohn@sujiths-mbp.home>
2021-11-06 17:54:12 -0400

    Add

---
+++ indextest.js
@@ -0,0 +1,36 @@
+
+const apiKey = 'MbMprHFPR_z5vwXJMwhXvn7lp3r05wDOJkAfByqxxN6U2PfI2XsPV6flgAW13YTbOyFoQyJGo98ygtOS8661ejHQ2Ws1iy3maW82HXeRQ5atFZSSPRYJuVxINFe_W3Yx';
+const url='https://api.yelp.com/v3/businesses/search'
+
+// import yelpAPI from 'yelp-api'
+
+
+// let yelp = new yelpAPI(apiKey);
+// let params = [{ location: 'winston salem' }];
+
+fetch('url',{
+  headers: {
+    Authorization: `bearer ${apiKey}`
+  }
+})
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  alert("Hello Javatpoint");  
+
+  // yelp.query('businesses/search', params)
+  // .then(data => {
+  //     var d = JSON.parse(data)
+  //     // console.log(d["businesses"])
+  //     console.log(d["businesses"][0].name)
+      
+  //     const t = d["businesses"][0].name;
+  //     document.getElementById('dataYelp').textContent = t;
+  // })
+  // .catch(err => {
+  //   // Failure
+  //   console.log(err);
+  // });  
+ 
+
+
