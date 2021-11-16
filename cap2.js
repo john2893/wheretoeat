86d68473d293229fb6442fea48808bb5dbf805b8
johns <johns@wfu.edu>
2021-11-16 16:47:57 -0500

    Add cap2.hs

--- cap2.js
+++ cap2.js
@@ -1,8 +1,10 @@
 
+const { response } = require('express');
+const express = require('express')
+const app = express()
 
 
-'use strict';
-
+app.use(express.static('public'));
 
 const apiKey = 'MbMprHFPR_z5vwXJMwhXvn7lp3r05wDOJkAfByqxxN6U2PfI2XsPV6flgAW13YTbOyFoQyJGo98ygtOS8661ejHQ2Ws1iy3maW82HXeRQ5atFZSSPRYJuVxINFe_W3Yx';
 const url='https://api.yelp.com/v3/businesses/search'
@@ -18,33 +20,48 @@ const searchRequest = {
   location: 'new york'
 };
 
+app.post('/api', (request, response) => {
+  console.log (request)
+  response.json({
+    status: "success",
+    them: "themy",
+    check: "all good"
+
+  })
+})
 const client = yelp.client(apiKey);
 
-client.search(searchRequest).then(response => {
-  const firstResult = response;
-  const reslen = firstResult.jsonBody.businesses.length;
-  const res = firstResult.jsonBody.businesses;
+// client.search(searchRequest).then(response => {
+//   const firstResult = response;
+//   const reslen = firstResult.jsonBody.businesses.length;
+//   const res = firstResult.jsonBody.businesses;
  
-  const data = JSON.stringify(res, null, 3);
-  console.log(res)
+//   const data = JSON.stringify(res, null, 3);
+//   //console.log(res)
   
-  for (let i=0; i<=(reslen-1); i++){
-    console.log(res[i].name)
-    console.log(res[i].image_url)
-    console.log(res[i].rating)
-    let catlen = res[i].categories.length
-    let catar = res[i].categories
-    //console.log(catar[0].alias)
-    catar.forEach((element, index) => {
-      console.log(element.alias)
+//   for (let i=0; i<=(reslen-1); i++){
+//     const name = (res[i].name)
+//     const img = (res[i].image_url)
+//     const rating = (res[i].rating)
+//     let catlen = res[i].categories.length
+//     let catar = res[i].categories
+//     //console.log(catar[0].alias)
+//     const catagory = []
+//     catar.forEach((element, index) => {
+//       catagory.push(element.alias)
 
      
-    });
-    //console.log(res[i].categories)
-    console.log("========================================")
-  }
+//     });
+    
+//     //console.log(res[i].categories)
+//     console.log("========================================")
+//   }
 
   
-}).catch(e => {
-  console.log(e);
+// }).catch(e => {
+//   console.log(e);
+// });
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost`)
 });
